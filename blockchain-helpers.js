783317dc63fefd4063eba57841851978c7880dbd
guyzilberblum <166912161+guyzilberblum@users.noreply.github.com>
2024-07-01 17:04:59 +0300

    Update blockchain-helpers.js

--- blockchain-helpers.js
+++ blockchain-helpers.js
@@ -1,3 +1,4 @@
+import sha256 from 'crypto-js/sha256.js';
 import { writeFileSync, readFileSync } from 'fs';
 
 export function writeBlockchain(blockchain) {
@@ -14,12 +15,36 @@ export function getBlockchain() {
 export function isValidChain() {
   const blockchain = getBlockchain();
 
+  // loop through blocks
   for (let i = 1; i < blockchain.length; i++) {
     const previousBlock = blockchain[i - 1];
-    const { previousHash } = blockchain[i];
+    const { hash, nonce, previousHash, transactions } = blockchain[i];
 
+    // validate previous hash
     if (previousHash !== previousBlock.hash) {
       return false;
+    }
+
+    // validate block hash
+    const testBlockHash = sha256(nonce + previousBlock.hash + JSON.stringify(transactions)).toString();
+    if (hash != testBlockHash) {
+      return false;
+    }
+
+    // loop through transactions
+    for (let j = 0; j < transactions.length; j++) {
+      const { hash, fromAddress, toAddress, amount } = transactions[j];
+
+      // don't validate reward transactions
+      if (fromAddress !== null) {
+
+        // validate transaction hash
+        const testTransactionHash = sha256(fromAddress + toAddress + amount).toString();
+        if (hash != testTransactionHash) {
+          return false;
+        }
+
+      }
     }
   }
 
@@ -35,4 +60,29 @@ export function getTransactions() {
   const transactionsFile = readFileSync('./transactions.json');
   const transactions = JSON.parse(transactionsFile);
   return transactions;
+}
+
+export function getAddressBalance(address) {
+  const blockchain = getBlockchain();
+  let balance = 0;
+
+  // loop through blocks
+  for (let i = 1; i < blockchain.length; i++) { 
+    const { transactions } = blockchain[i];
+
+    // loop through transactions
+    for (let j = 0; j < transactions.length; j++) {
+      const { fromAddress, toAddress, amount } = transactions[j];
+
+      if (fromAddress === address) {
+        balance -= amount;
+      }
+
+      if (toAddress === address) {
+        balance += amount;
+      }
+    }
+  }
+
+  return balance;
 }
